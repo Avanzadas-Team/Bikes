d59a48d684a5b248f3b2ac46fec6731a0c0de88c
Francisco <fmurillom@gmail.com>
2020-10-18 22:13:00 -0600

    [Add] Fragmentation

---
+++ mariadb/Scripts/California/DBSetup.sql
@@ -0,0 +1,11 @@
+CHANGE REPLICATION FILTER
+    REPLICATE_DO_TABLE = (produccion.categorias, produccion.marcas, produccion.productos, ventas.clientes, ventas.tiendaCalifornia, ventas.empleadosCalifornia, ventas.ordenesCalifornia, ventas.detalleOrdenCalifornia, produccion.inventarioCalifornia, ventas.ordenes);
+
+CHANGE MASTER TO MASTER_HOST='NewYorkDB',
+    MASTER_USER='repl',
+    MASTER_PASSWORD='password',
+    MASTER_LOG_FILE='Inserte el nombre del LOGFile obtenido del Master',
+    MASTER_LOG_POS= Insertar el pos obtenido del master;
+
+start slave;
+show slave status;
