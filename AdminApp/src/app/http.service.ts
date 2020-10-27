8a7ec2215d4a1bb142b9352cce8eed3e827c9cab
kgonzalez712 <kevin.gonzalezs0795@gmail.com>
2020-10-26 23:28:27 -0600

    [Fix] API Url changes

--- AdminApp/src/app/http.service.ts
+++ AdminApp/src/app/http.service.ts
@@ -7,19 +7,22 @@ import { Injectable } from '@angular/core';
 export class HttpService {
 
   constructor(private http: HttpClient) { }
-  BaseURL = 'https://localhost:5001'
+  BaseURLNY = 'https://localhost:5001';
+  BaseURLCA = 'https://localhost:5003';
+  BaseURLTX = 'https://localhost:5005';
+
 
   getProducts() {
-    return this.http.get(this.BaseURL + '/sa/prods');
+    return this.http.get(this.BaseURLNY + '/sa/prods');
   }
   getProdNY(id: string, dateS: string, dateE: string) {
-    return this.http.get(this.BaseURL + '/sa/NY/' + id + '/' + dateS + '/' + dateE);
+    return this.http.get(this.BaseURLNY + '/sa/NY/' + id + '/' + dateS + '/' + dateE);
   }
   getProdCA(id: string, dateS: string, dateE: string) {
-    return this.http.get(this.BaseURL + '/sa/CA/' + id + '/' + dateS + '/' + dateE);
+    return this.http.get(this.BaseURLCA + '/sa/CA/' + id + '/' + dateS + '/' + dateE);
   }
   getProdTX(id: string, dateS: string, dateE: string) {
-    return this.http.get(this.BaseURL + '/sa/TX/' + id + '/' + dateS + '/' + dateE);
+    return this.http.get(this.BaseURLTX + '/sa/TX/' + id + '/' + dateS + '/' + dateE);
   }
 
 }
