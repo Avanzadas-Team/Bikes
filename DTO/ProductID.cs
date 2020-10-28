1e94975ac9ad7bb64149d89cd70ba7e3041bc0cb
kevin9907 <kevin0607as@gmail.com>
2020-10-27 23:01:28 -0600

    [fix] dockerfile

---
+++ DTO/ProductID.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Bikes.DTO
+{
+    public class ProductID
+    {
+        public string name;
+
+        public int idProduct;
+
+        public decimal price;
+    }
+}
